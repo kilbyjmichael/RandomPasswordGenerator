d21af4e9f8b7f419124f51139772871bcac1c847
Michael <kilbyjmichael@gmail.com>
2015-03-19 22:27:39 -0500

    2.1 Updated the JavaDoc comments

--- RandomPasswordGenerator.java
+++ RandomPasswordGenerator.java
@@ -1,9 +1,11 @@
 package com.kilby;
 
 /**
- * A class that generates a random password according to a set of rules.
+ * The RandomPasswordGenerator generates a password that consists of
+ * letters and numbers using the psudo-random Random class.
  *
  * @author Michael Kilby
+ * @version 2.1 Mar 19, 2015
  *
  */
 public class RandomPasswordGenerator
@@ -12,12 +14,25 @@ public class RandomPasswordGenerator
 	private StringBuilder password = new StringBuilder();
 	private String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
+	/**
+	 * The randomNumber method generates a random number using a given range.
+	 * 
+	 * @param min This is the minimum number of the random range.
+	 * @param max This is the maximum number of the random range.
+	 * @return int This returns a psudo-random number using the range of max - min.
+	 */
 	private int randomNumber(int min, int max)
 	{
 	   int range = (max - min) + 1;
 	   return (int)(Math.random() * range) + min;
 	}
 	
+	/**
+	 * The generateNumbers method generates an amount of numbers according to a given percentage.
+	 * 
+	 * @param percent This is the percent of the password that is numbers.
+	 * @return String This returns the number part of the password.
+	 */
 	private String generateNumbers(double percent)
 	{
 		StringBuilder numPass = new StringBuilder();
@@ -30,6 +45,12 @@ public class RandomPasswordGenerator
 		return numPass.toString();
 	}
 	
+	/**
+	 * The generateChars method generates an amount of letters according to a given percentage.
+	 * 
+	 * @param percent This is the percent of the password that is characters.
+	 * @return String This returns the character part of the password.
+	 */
 	private String generateChars(double percent)
 	{
 		StringBuilder charPass = new StringBuilder();
@@ -43,6 +64,15 @@ public class RandomPasswordGenerator
 		return charPass.toString();
 	}
 	
+	/**
+	 * The generatePassword method generates a string using given numbers and an order.
+	 * 
+	 * @param length This is the length of the password.
+	 * @param numPercent This is the percent of the password that is numbers.
+	 * @param charPercent This is the percent of the password that is letters.
+	 * @param order This is the order that numbers and letters come in the password. "numbers" means numbers first, "chars" means letters first.
+	 * @return String This is the final password.
+	 */
 	public String generatePassword(int length, double numPercent, double charPercent, String order)
 	{
 		//clear vars
